f0ab1746ed574b1ca27f0def3a4c798d6ee068f8
sgiffard <samuel.giffard@unleashed.be>
2018-10-16 22:45:38 +0200

    Added sessions 3 and 4.

--- verbs/data.py
+++ verbs/data.py
@@ -238,5 +238,254 @@ verbs: List[Set[Verb]] = [
             perfectum=Perfectum(Auxilaries.ZIJN, "opgestaan"),
             english_translation="to get up / to stand up",
         ),
+    },
+    {
+        Verb(
+            "oversteken",
+            perfectum=Perfectum(Auxilaries.ZIJN, "overgestoken"),
+            english_translation="to cross over",
+        ),
+        Verb(
+            "rijden",
+            perfectum=Perfectum(Auxilaries.BOTH, "gereden"),
+            english_translation="to drive",
+        ),
+        Verb(
+            "roepen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "geroepen"),
+            english_translation="to call",
+        ),
+        Verb(
+            "scheiden",
+            perfectum=Perfectum(Auxilaries.ZIJN, "gescheiden"),
+            english_translation="to divorce",
+        ),
+        Verb(
+            "schijnen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "geschenen"),
+            english_translation="to shine",
+        ),
+        Verb(
+            "schrijven",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "geschreven"),
+            english_translation="to write",
+        ),
+        Verb(
+            "slapen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "geslapen"),
+            english_translation="to sleep",
+        ),
+        Verb(
+            "sluiten",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gesloten"),
+            english_translation="to close",
+        ),
+        Verb(
+            "snijden",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gesneden"),
+            english_translation="to cut",
+        ),
+        Verb(
+            "spreken",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gesproken"),
+            english_translation="to speak",
+        ),
+        Verb(
+            "springen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gesprongen"),
+            english_translation="to jump",
+        ),
+        Verb(
+            "staan",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gestaan"),
+            english_translation="to stand",
+        ),
+        Verb(
+            "steken",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gestoken"),
+            english_translation="to stab",
+        ),
+        Verb(
+            "sterven",
+            perfectum=Perfectum(Auxilaries.ZIJN, "gestorven"),
+            english_translation="to die",
+        ),
+        Verb(
+            "strijken",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gestreken"),
+            english_translation="to iron",
+        ),
+        Verb(
+            "treffen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "getroffen"),
+            english_translation="to meet",
+        ),
+        Verb(
+            "trekken",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "getrokken"),
+            english_translation="to pull",
+        ),
+        Verb(
+            "uitdoen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "uitgedaan"),
+            english_translation="to turn off",
+        ),
+        Verb(
+            "uitgaan",
+            perfectum=Perfectum(Auxilaries.ZIJN, "uitgegaan"),
+            english_translation="to go out",
+        ),
+        Verb(
+            "uittrekken",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "uitgetrokken"),
+            english_translation="to draw / to pull out / to extract",
+        ),
+        Verb(
+            "vallen",
+            perfectum=Perfectum(Auxilaries.ZIJN, "gevallen"),
+            english_translation="to fall",
+        ),
+        Verb(
+            "varen",
+            perfectum=Perfectum(Auxilaries.BOTH, "gevaren"),
+            english_translation="to sail",
+        ),
+        Verb(
+            "verbieden",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "verboden"),
+            english_translation="to forbid",
+        ),
+        Verb(
+            "vergeten",
+            perfectum=Perfectum(Auxilaries.BOTH, "vergeten"),
+            english_translation="to forget",
+        ),
+        Verb(
+            "verkopen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "verkocht"),
+            english_translation="to sell",
+        ),
+    },
+    {
+        Verb(
+            "verliezen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "verloren"),
+            english_translation="to lose",
+        ),
+        Verb(
+            "verstaan",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "verstaan"),
+            english_translation="to understand",
+        ),
+        Verb(
+            "vertrekken",
+            perfectum=Perfectum(Auxilaries.ZIJN, "vertrokken"),
+            english_translation="to leave",
+        ),
+        Verb(
+            "vervangen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "vervangen"),
+            english_translation="to replace",
+        ),
+        Verb(
+            "vinden",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gevongen"),
+            english_translation="to find",
+        ),
+        Verb(
+            "vliegen",
+            perfectum=Perfectum(Auxilaries.BOTH, "gevlogen"),
+            english_translation="to travel by plane",
+        ),
+        Verb(
+            "voorkomen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "voorkomen"),
+            english_translation="to prevent",
+        ),
+        Verb(
+            "vragen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gevraagd"),
+            english_translation="to ask",
+        ),
+        Verb(
+            "vriezen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gevroren"),
+            english_translation="to freeze",
+        ),
+        Verb(
+            "wassen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gewassen"),
+            english_translation="to wash",
+        ),
+        Verb(
+            "wegen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gewogen"),
+            english_translation="to weigh",
+        ),
+        Verb(
+            "weten",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "geweten"),
+            english_translation="to know",
+        ),
+        Verb(
+            "wijzen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gewezen"),
+            english_translation="to point / to show",
+        ),
+        Verb(
+            "willen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gewild"),
+            english_translation="to want",
+        ),
+        Verb(
+            "winnen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gewonnen"),
+            english_translation="to win",
+        ),
+        Verb(
+            "worden",
+            perfectum=Perfectum(Auxilaries.ZIJN, "geworden"),
+            english_translation="to become",
+        ),
+        Verb(
+            "zeggen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gezegd"),
+            english_translation="to say",
+        ),
+        Verb(
+            "zien",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gezien"),
+            english_translation="to see",
+        ),
+        Verb(
+            "zijn",
+            perfectum=Perfectum(Auxilaries.ZIJN, "geweest"),
+            english_translation="to be",
+        ),
+        Verb(
+            "zingen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gezongen"),
+            english_translation="to sing",
+        ),
+        Verb(
+            "zitten",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gezeten"),
+            english_translation="to sit",
+        ),
+        Verb(
+            "zoeken",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "gezocht"),
+            english_translation="to search",
+        ),
+        Verb(
+            "zullen",
+            perfectum=Perfectum(Auxilaries.HEBBEN, "-"),
+            english_translation="to shall",
+        ),
+        Verb(
+            "zwemmen",
+            perfectum=Perfectum(Auxilaries.BOTH, "gezwommen"),
+            english_translation="to swim",
+        ),
     }
 ]
